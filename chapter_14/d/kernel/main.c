0834520cdc3b34840c09a2548d5ae9c6933d8694
xukanshan <1085714254@qq.com>
2023-09-02 15:43:35 +0800

    第14章，d小节

---
+++ chapter_14/d/kernel/main.c
@@ -0,0 +1,16 @@
+#include "print.h"
+#include "init.h"
+#include "fs.h"
+#include "stdio.h"
+
+
+int main(void) {
+   put_str("I am kernel\n");
+   init_all();
+   uint32_t fd = sys_open("/file1", O_RDONLY);
+   printf("fd:%d\n", fd);
+   sys_close(fd);
+   printf("%d closed now\n", fd);
+   while(1);
+   return 0;
+}
